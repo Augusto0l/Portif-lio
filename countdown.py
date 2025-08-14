336bd97488e3daebdb3d434e6c8435436127f41e
Pedro Augusto <augustolourenco100@gmail.com>
2025-08-13 21:59:30 -0300

    Create countdown.py
    Esse é meu projeto: um contador regressivo (timer) feito em Python:
    O programa pede para o usuário informar um tempo em segundos, verifica se o valor é numérico e, caso seja válido, converte para um número inteiro. Em seguida, ele conta regressivamente até chegar a zero, exibindo o tempo restante no formato “minutos:segundos” e atualizando a cada segundo. Quando o tempo acaba, aparece a mensagem "TEMPO ACABOU!".

---
+++ countdown.py
@@ -0,0 +1,18 @@
+import time
+
+t = input("Digite o tempo (em segundos): ")
+
+if t.isdigit():
+    t = int(t)
+else:
+    print("Entrada invalida!")
+    quit()
+
+while t:
+    minutes, seconds = divmod(t, 60)
+    timer = f"{minutes}:{seconds}"
+    print(timer, end="\r")
+    time.sleep(1)
+    t = t - 1
+
+print("TEMPO ACABOU!")
